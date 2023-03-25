a10538d10a666ee770030c1e967c3e385eac15e0
fishertom <tfisher92@gmail.com>
2023-03-25 22:37:08 +0000

    fix use effect in combobox

--- src/components/ComboBox.jsx
+++ src/components/ComboBox.jsx
@@ -19,7 +19,7 @@ function ComboBox({ items }) {
       setFilteredItems([]);
       setShowDropdown(false);
     }
-  }, [inputText]);
+  }, [inputText, items]);
 
   return (
     <div className="p-2 flex flex-col gap-2">
