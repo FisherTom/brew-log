1a62287f492ba30d9eaf8669cfaf00f1e6e63bde
fishertom <tfisher92@gmail.com>
2023-03-30 17:01:19 +0100

    update landing page

--- src/components/HomeSheet.jsx
+++ src/components/HomeSheet.jsx
@@ -3,49 +3,37 @@ import React from "react";
 function HomeSheet() {
   return (
     <div>
-      <div className="flex flex-col w-[90%] max-w-6xl gap-5 p-6 m-4 bg-white shadow-2xl h-fit">
+      <div className="flex flex-col max-w-6xl gap-5 p-6 m-4 bg-white shadow-2xl h-fit">
         <div>
-          <div className="flex items-end justify-between " id="sheet-title">
-            <h1 className="flex flex-wrap text-6xl font-bebas drop-shadow-[2px_2px_3px_rgba(160,160,160)]">
-              Welcome To Bru-Sheets!
-            </h1>
-            <h3 className="text-3xl font-bebas">
-              Portfolio project by Tom Fisher
-            </h3>
-          </div>
-          <div className="flex justify-between">
-            <img
-              alt="barley"
-              src={require("../png-icons/barley.png")}
-              className="w-20 h-20 m-10"
-            />
-            <img
-              alt="hop"
-              src={require("../png-icons/hop.png")}
-              className="w-20 h-20 m-10"
-            />
-            <img
-              alt="brew"
-              src={require("../png-icons/production.png")}
-              className="w-20 h-20 m-10"
-            />
-            <img
-              alt="beer"
-              src={require("../png-icons/beer1.png")}
-              className="w-20 h-20 m-10"
-            />
+          <div className="flex justify-between gap-5">
+            <div className="flex flex-col " id="sheet-title">
+              <h1 className="flex flex-wrap text-6xl font-bebas drop-shadow-[2px_2px_3px_rgba(160,160,160)]">
+                Welcome To Bru-Sheets!
+              </h1>
+              <h3 className="text-3xl font-bebas">
+                Portfolio project by Tom Fisher
+              </h3>
+            </div>
             <img
               alt="cheers"
               src={require("../png-icons/cheers(1).png")}
-              className="w-20 h-20 m-10"
+              className="w-20 h-20"
             />
           </div>
           <div className="border-black flexborder-t-2">
-            <p className="p-2">
+            <p className="p-2 mt-10">
               Bru-Sheets keeps you organized by storing all of your brewing
               recipies and brew logs in one place. Get started by selecting a
               beer recipe or jump right in and create you own using the
-              template.
+              template. <br />
+              <br />
+              This is a portfolio project by Tom fisher, you can find more info
+              on this and other projects on my portfolio site -
+              <a className="text-pink-600" href="https://tomfisher.dev/">
+                {" "}
+                tomfisher.dev{" "}
+              </a>
+              .
             </p>
           </div>
         </div>
